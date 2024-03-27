976d6cf3070a976971a36ad5d53fe424998314e8
GDayan <daykanot@gmail.com>
2024-03-27 11:37:26 +0300

    done final task

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        long result = 1;
 
+        // If power is negative, invert numberToPrint and power
+        if (power < 0) {
+            numberToPrint = -numberToPrint;
+            power = -power;
+        }
+
+        // Calculate power using a for loop
+        for (int i = 0; i < power; i++) {
+            result *= numberToPrint;
+        }
+
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,23 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        // Loop through each row of the pyramid
+        for (int i = 1; i <= cathetusLength; i++) {
+            // Print spaces to align the numbers properly
+            for (int j = 0; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            // Print numbers in ascending order
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            // Print numbers in descending order
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            // Move to the next line after printing each row
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
